f6fa4cc16a08d2e6f8dab2ea78eb0cf3020fb856
Stefanie <stefaniepires91@gmail.com>
2022-03-03 15:26:08 -0600

    created instance variables for user class

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 
 // Entity: marks this object as a persistable object so that the user can map to a table */
 // Json Ignore Props: specifies properties that should be ignored when serializing the object to JSON
-// Table: the name of the table that this class maps to 
+// Table: the name of the table that this class maps to
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -16,5 +16,15 @@ import javax.persistence.Table;
 
 
 public class User {
+//instance variables
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
 
+    //lists are collections of objects of the same type
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
 }
